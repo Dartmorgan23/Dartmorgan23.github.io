544f890886179dd68d31c3454cf41fee544c4728
Eduard <vrastorguevv@gmail.com>
2018-06-06 23:55:17 +0300

    image

--- WOODER/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
- 
-gulp.task('compress', function() {
-  gulp.src('img/*')
-  .pipe(imagemin())
-  .pipe(gulp.dest('img2/'))
-});
